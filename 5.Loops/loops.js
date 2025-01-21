34d27ed538712657a5b1d66657e4f1e516cb8c2c
Diya <148466419+Diya0077@users.noreply.github.com>
2025-01-21 23:56:00 +0530

    commit

---
+++ 5.Loops/loops.js
@@ -0,0 +1,32 @@
+// Why learn this?
+// Loops help repeat tasks efficiently.
+
+console.log("This is on Loops")
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+
+// for loop:
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  
+  // forEach loop:
+  [1, 2, 3].forEach(num => console.log(num));
+  
+  // while loop:
+  let count = 0;
+  while (count < 3) {
+    console.log(count);
+    count++;
+  }
+  
+  // do-while loop:
+  do {
+    console.log(count);
+    count--;
+  } while (count > 0);
+  
